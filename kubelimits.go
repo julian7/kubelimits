dc5ac05d1a829045b30946030f1c90565c42f6f6
Balazs Nagy <julsevern@gmail.com>
2025-03-05 08:17:04 +0100

    documentation

--- kubelimits.go
+++ kubelimits.go
@@ -4,12 +4,16 @@ import (
 	"errors"
 )
 
+// ErrScan is an error value Set() returns with, when
+// cgroup2 limit files cannot be parsed.
 var ErrScan = errors.New("Scan error")
 
 type setter struct {
 	loggers []func(string)
 }
 
+// Set reads cgroup2 cpu.max and memory.max, and sets GOMAXPROCS and runtime
+// memory limit accordingly. It is a no-op on non-linux environments.
 func Set(loggers ...func(string)) error {
 	s := &setter{loggers: loggers}
 	if err := s.SetMemory(); err != nil {
